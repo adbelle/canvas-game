30bca43b90346096321c549a6e7ee554ee02292c
Anthony Bellew <abellew@integrate.com>
2019-06-02 17:29:19 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Hi! This is a simple starting point for a HTML5 game using Canvas, Sockets.io and Express.
+
+You'll need Node and NPM. I'm using the NodeJS.org LTS of 10.16.0 and npm 6.9.0.
+
+To start using this, here's what you'll have to do:
+
+* `npm install`
+* `npm start`
+
+The start script runs webpack in watch mode, and nodemon, so any changes you make to the client or server directories should cause webpack to rebuild and the server to restart, respectively. Note that you may need to wait a moment for the webpack build to complete - if you refresh the page too soon, you may not see your changes.
---
+++ server/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 3000;
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+
+app.use(morgan('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Static middleware
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.get('/*', (req, res, next) => {
+	res.sendFile(path.join(__dirname, '..', 'index.html'));
+});
+
+app.use((req, res, next) => {
+	const err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+app.use((err, req, res, next) => {
+	res.status(err.status || 500);
+	res.send(err.message || 'Internal server error');
+});
+
+io.on('connection', (socket) => {
+	console.log('a user connected:', socket.id);
+
+	socket.on('disconnect', function() {
+		console.log('user disconnected');
+	});
+});
+
+server.listen(PORT, () => {
+	console.log('Server is live on PORT:', PORT);
+});
